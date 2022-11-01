c487cee1b27f514ee863805df3722651132c2a35
gghtn <ghtn9898@gmail.com>
2022-11-02 03:16:27 +0900

    main

--- WP_4/drawing_project.cpp
+++ WP_4/drawing_project.cpp
@@ -9,7 +9,6 @@
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-int g_nWidth, g_nHeight;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -97,30 +96,51 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     return TRUE;
 }
 
+
+HWND hScroll[7];                        //펜RGB, 브러시RGB, 두께
+HINSTANCE g_hInst;                      //스크롤바 윈도우 인스턴트핸들
+int nScroll[7];                         //펜RGB 브러시RGB 좌표, 두께 nScroll[6] => width
+int g_nWidth, g_nHeight;                //클라이언트 창 너비, 높이
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     HDC hdc;
     PAINTSTRUCT ps;
     HPEN myPen, oldPen;
     HBRUSH myBrush, oldBrush;
-    COLORREF penColor = 0, brushColor = 0;
+    static COLORREF penColor = 0, brushColor = 0;   //정적변수가 아닐시 루프를 돌면서 계속 실행됨 -> 0으로 값이 고정
     TEXTMETRIC tm;
-    RECT textRect, posRect;
+    RECT tmpRect, posRect;
 
     char text1[] = "기본 텍스트", text2[] = "투명 텍스트", text3[] = "불투명 텍스트";
     //WM_PRINT의 x,y, 텍스트의 두께, 그리기모드, 두께
-    static int x = 30, y = 500, tW1, tW2, tW3, cyChar = 0, drawMode = NOT, width = 1;
+    static int x = 30, y = 500, tW1, tW2, tW3, cyChar = 0, drawMode = NOT;
 
     static char string[201];       //문자열
     static int len = 0, reStrX;    //문자열 길이, 길이가 200을 넘을 경우 새로 그릴 x좌표
 
-    static int penStyle = PS_SOLID, brushStyle = 6, textStyle = -1;
-    static bool bDrawing = false; 
+    static int penStyle = PS_SOLID, brushStyle = 6, textStyle = -1, width;
+    static bool bDrawing = false, bWidth = false, bFull = false;  //드로잉 영역을 클릭했는지, 두께 스크롤 창을 클릭했는지, 전체지우기인지
     static short x1, y1, x2, y2;
+    int tmpPos; //
 
     switch (message)
     {
     case WM_CREATE:
+        for (int i = 0; i < 3; i++) {   //펜 스크롤
+            hScroll[i] = CreateWindow("scrollbar", NULL, WS_CHILD | WS_VISIBLE | SBS_HORZ, 800, 500 + i*40, 200, 20, hWnd, NULL, g_hInst, NULL);
+            SetScrollRange(hScroll[i], SB_CTL, 0, 255, TRUE);
+            SetScrollPos(hScroll[i], SB_CTL, 0, TRUE);
+        }
+        for (int i = 0; i < 3; i++) {   //브러쉬 스크롤
+            hScroll[i+3] = CreateWindow("scrollbar", NULL, WS_CHILD | WS_VISIBLE | SBS_HORZ, 1050, 500 + i * 40, 200, 20, hWnd, NULL, g_hInst, NULL);
+            SetScrollRange(hScroll[i+3], SB_CTL, 0, 255, TRUE);
+            SetScrollPos(hScroll[i+3], SB_CTL, 0, TRUE);
+        }
+        //두께 스크롤
+        hScroll[6] = CreateWindow("scrollbar", NULL, WS_CHILD | WS_VISIBLE | SBS_HORZ, 1050, 640, 200, 20, hWnd, NULL, g_hInst, NULL);
+        SetScrollRange(hScroll[6], SB_CTL, 1, 30, TRUE);
+        SetScrollPos(hScroll[6], SB_CTL, 1, TRUE);
 
         hdc = GetDC(hWnd);
         GetTextMetrics(hdc, &tm);
@@ -135,6 +155,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
         g_nWidth = LOWORD(lParam);
         g_nHeight = HIWORD(lParam);
+
         break;
     case WM_SIZE:
         g_nWidth = LOWORD(lParam);
@@ -147,6 +168,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         HDC hdc = BeginPaint(hWnd, &ps);
         MoveToEx(hdc, 0, 490, NULL);
         LineTo(hdc, g_nWidth, 490);
+        //스크롤바 텍스트, 지우개 그리기
+        {
+            tmpRect = { 800, 610, 1000, 630 };
+            DrawText(hdc, "펜RGB", 5, &tmpRect, DT_CENTER);
+            tmpRect = { 1050, 610, 1250, 630 };
+            DrawText(hdc, "브러쉬RGB", 9, &tmpRect, DT_CENTER);
+            tmpRect = { 1050, 670, 1250, 690 };
+            DrawText(hdc, "펜 굵기", 7, &tmpRect, DT_CENTER);
+
+            tmpRect = { ERASER_X1, ERASER_Y1, ERASER_X2, ERASER_Y2 };
+            Rectangle(hdc, tmpRect.left, tmpRect.top, tmpRect.right, tmpRect.bottom);
+            DrawText(hdc, "\n\n지우개", 8, &tmpRect, DT_CENTER | DT_WORDBREAK);  //센터정렬, 오른쪽 끝에서 개행
+
+            tmpRect = { FULL_X1, ERASER_Y1, FULL_X2, ERASER_Y2 };
+            Rectangle(hdc, tmpRect.left, tmpRect.top, tmpRect.right, tmpRect.bottom);
+            DrawText(hdc, "\n전체\n지우개", 12, &tmpRect, DT_CENTER | DT_WORDBREAK);
+
+        }
         //펜, 직선, 원, 사각형, 라운드사각형 그리기
         for (int i = 0; i < 5; i++) {
             tmpX = x + i * 60;
@@ -185,17 +224,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             tmpX = x + i * 60;
             SetBrush(hdc, myBrush, oldBrush, i, 0);
             Ellipse(hdc, tmpX, 640, tmpX + 50, 690);
+            SelectObject(hdc, oldBrush);
+            DeleteObject(myBrush);
         }
         // 텍스트 그리기
         {
-            textRect = { 350, y + 15, 350 + tW1, y + 15 + cyChar };
-            DrawText(hdc, text1, strlen(text1), &textRect, DT_CENTER);
+            tmpRect = { 350, y + 15, 350 + tW1, y + 15 + cyChar };
+            DrawText(hdc, text1, strlen(text1), &tmpRect, DT_CENTER);
 
-            textRect = { 350, y + 55, 350 + tW2, y + 55 + cyChar };
-            DrawText(hdc, text2, strlen(text2), &textRect, DT_CENTER);
+            tmpRect = { 350, y + 55, 350 + tW2, y + 55 + cyChar };
+            DrawText(hdc, text2, strlen(text2), &tmpRect, DT_CENTER);
 
-            textRect = { 350, y + 95, 350 + tW3, y + 95 + cyChar };
-            DrawText(hdc, text3, strlen(text3), &textRect, DT_CENTER);
+            tmpRect = { 350, y + 95, 350 + tW3, y + 95 + cyChar };
+            DrawText(hdc, text3, strlen(text3), &tmpRect, DT_CENTER);
         }
         // 미리보기 박스
         if (drawMode) {
@@ -239,13 +280,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     break;
                 }
                 break;
+            default:
+                break;
             }
-            //?
+            //당장에 문제가 없어보여도 복구와 삭제는 반드시 한다. -> 계속 생성만되면 문제발생
             SelectObject(hdc, oldPen);
             SelectObject(hdc, oldBrush);
             DeleteObject(myPen);
             DeleteObject(myBrush);
         }
+
+        //전체지우기 클릭시
+        if (bFull) {    
+            bFull = false;
+            SetPen(hdc, myPen, oldPen, penStyle, 1, RGB(255, 255, 255));
+            SetBrush(hdc, myBrush, oldBrush, 5, RGB(255, 255, 255));
+            Rectangle(hdc, 0, 0, g_nWidth, 490);
+            SelectObject(hdc, oldPen);
+            SelectObject(hdc, oldBrush);
+            DeleteObject(myPen);
+            DeleteObject(myBrush);
+        }
+
         EndPaint(hWnd, &ps);
         break;
     }
@@ -255,7 +311,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         y1 = y2 = HIWORD(lParam);
 
         if (y1 < 490) { //그리기 영역을 클릭한경우
-            if (drawMode == PEN || drawMode == TEXT) {
+            if (drawMode == PEN || drawMode == TEXT || drawMode == ERASER) {
                 SetCursor(LoadCursor(NULL, IDC_ARROW));
             }
             else {
@@ -299,6 +355,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     textStyle = tmp;
                     break;
                 }
+
+                bFull = SelectFull(x1, y1);
                 break;
             }
             InvalidateRect(hWnd, NULL, false);  //무효화 WM_PAINT실행
@@ -316,7 +374,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     MoveToEx(hdc, x1, y1, NULL);
                     x1 = LOWORD(lParam);
                     y1 = HIWORD(lParam);
-                    if (y1 > 490) y1 = 490;
+                    if (y1 > 490 - width/2) y1 = 490 - width / 2;
+                    LineTo(hdc, x1, y1);
+                }
+            }
+            else if (drawMode == ERASER) {
+                SelectObject(hdc, oldPen);
+                DeleteObject(myPen);
+                SetPen(hdc, myPen, oldPen, penStyle, width, RGB(255,255,255));
+                if (GetCapture() == hWnd) {
+                    MoveToEx(hdc, x1, y1, NULL);
+                    x1 = LOWORD(lParam);
+                    y1 = HIWORD(lParam);
+                    if (y1 > 490 - width / 2) y1 = 490 - width / 2;
                     LineTo(hdc, x1, y1);
                 }
             }
@@ -326,11 +396,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
                 x2 = LOWORD(lParam);
                 y2 = HIWORD(lParam);
-                if (y2 > 490) y2 = 490;
+                if (y2 > 490 - width / 2) y2 = 490 - width / 2;
 
                 posRect = { x1, y1, x2, y2 };
                 DrawFigure(hdc, drawMode, posRect, false);
             }
+            SelectObject(hdc, oldPen);
+            SelectObject(hdc, oldBrush);
+            DeleteObject(myPen);
+            DeleteObject(myBrush);
+
             ReleaseDC(hWnd, hdc);
         }
         
@@ -339,14 +414,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_LBUTTONUP:
         if (bDrawing) {
             hdc = GetDC(hWnd);
-            SetPen(hdc, myPen, oldPen, penStyle, width, penColor);
-            SetBrush(hdc, myBrush, oldBrush, brushStyle, brushColor);
+            if (drawMode == ERASER) {
+                ReleaseCapture();
+            }
+            else {
+                SetPen(hdc, myPen, oldPen, penStyle, width, penColor);
+                SetBrush(hdc, myBrush, oldBrush, brushStyle, brushColor);
 
-            posRect = { x1, y1, x2, y2 };
-            ReleaseCapture();
-            bDrawing = false;               
-            DrawFigure(hdc, drawMode, posRect, true);
+                posRect = { x1, y1, x2, y2 };
+                ReleaseCapture();
+                bDrawing = false;
+                DrawFigure(hdc, drawMode, posRect, true);
 
+                SelectObject(hdc, oldPen);
+                SelectObject(hdc, oldBrush);
+                DeleteObject(myPen);
+                DeleteObject(myBrush);
+            }
+            
             ReleaseDC(hWnd, hdc);
         }
         break;
@@ -388,12 +473,54 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 memset(string, '\0', 200);
                 len = 0;
             }
-           
-
             ReleaseDC(hWnd, hdc);
         }
         break;
 
+    case WM_HSCROLL:
+        for (int i = 0; i < 7; i++) {
+            if ((HWND)lParam == hScroll[i]) {       //클릭한 창이 스크롤 창일 경우 스크롤 창의 좌표를 구함
+                tmpPos = nScroll[i];
+                if (i == 6) bWidth = true;
+                break;
+            }
+        }
+        switch (LOWORD(wParam)) {           //wParam 하위 2바이트 = 눌린버튼
+        case SB_LINELEFT:
+            tmpPos = max(0, tmpPos - 1);
+            break;
+        case SB_LINERIGHT:
+            if(bWidth)  tmpPos = min(30, tmpPos + 1);   //두께 스크롤 창을 클릭한 경우 최대크기가 50을 넘지 않음
+            else tmpPos = min(255, tmpPos + 1);
+            break;
+        case SB_PAGELEFT:
+            tmpPos = max(0, tmpPos - 10);
+            break;
+        case SB_PAGERIGHT:
+            if (bWidth)  tmpPos = min(30, tmpPos + 10);
+            else tmpPos = min(255, tmpPos + 10);
+            break;
+        case SB_THUMBTRACK:
+            tmpPos = HIWORD(wParam);        //wParam 상위 2바이트 = thumb위치
+            break;
+        }
+        for (int i = 0; i < 7; i++) {
+            if ((HWND)lParam == hScroll[i]) {       //다시 클릭한 창을 구해 좌표 갱신
+                nScroll[i] = tmpPos;
+                break;
+            }
+        }
+
+        penColor = RGB(nScroll[0], nScroll[1], nScroll[2]);
+        brushColor = RGB(nScroll[3], nScroll[4], nScroll[5]);
+        width = nScroll[6];
+        bWidth = false;
+
+        SetScrollPos((HWND)lParam, SB_CTL, tmpPos, TRUE);  //thumb의 위치 재설정
+        InvalidateRect(hWnd, NULL, false);
+
+        break;
+   
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
